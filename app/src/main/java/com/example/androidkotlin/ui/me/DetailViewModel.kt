85d76ac3bc78978e0a82d524e9587e13f82ae622
liuzonghai8 <liuzonghai8@github.com>
2021-03-10 15:49:54 +0800

    add

---
+++ app/src/main/java/com/example/androidkotlin/ui/me/DetailViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.androidkotlin.ui.me
+
+import androidx.lifecycle.ViewModel
+
+class DetailViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
--- app/src/main/java/com/example/androidkotlin/ui/me/MeFragment.kt
+++ app/src/main/java/com/example/androidkotlin/ui/me/MeFragment.kt
@@ -1,8 +1,11 @@
 package com.example.androidkotlin.ui.me
 
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.example.androidkotlin.R
 import com.example.androidkotlin.base.BaseViewModelFragment
 import com.example.androidkotlin.databinding.MeFragmentBinding
+import kotlinx.android.synthetic.main.me_fragment.view.*
 
 class MeFragment :BaseViewModelFragment<MeFragmentBinding,MeViewModel>() {
 
@@ -11,5 +14,11 @@ class MeFragment :BaseViewModelFragment<MeFragmentBinding,MeViewModel>() {
        return MeViewModel::class.java
     }
 
+    override fun initView(view: View) {
+        super.initView(view)
+        view.button.setOnClickListener{
+            findNavController().navigate(R.id.action_meFragment_to_detailFragment)
+        }
+    }
 
 }
